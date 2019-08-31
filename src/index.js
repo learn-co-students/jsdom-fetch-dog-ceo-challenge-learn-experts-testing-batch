17944c16cef70b3cfddbd170663efaf827e24ee6
Madeline-Stark <madeline.stark1@gmail.com>
2019-08-31 14:56:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
-console.log('%c HI', 'color: firebrick')
+var breedArray = []
+
+window.onload = () => {
+    getDogs()
+    getBreeds()
+    displayBreeds()
+    onBreedClick()
+    onFilterClick()
+}
+
+function getDogs() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  .then(function(response) {
+    return response.json();
+    })
+  .then(function(json) {
+    const dogArray = json.message;
+    let imgContainer = document.getElementById("dog-image-container"); //can't be const b/c change below
+    dogArray.forEach(function(dog){
+      //for each loop to add to dom
+      imgContainer.innerHTML += `<img src=${dog} />`
+      //have to use innerHTML instead of innerText or else links it will show up as text, not imgs
+    })
+    });
+}
+
+function getBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+  .then(function(response) {
+    return response.json();
+    })
+  .then(function(json) {
+    let breeds = Object.keys(json.message); //returns an array of a given object's own enumerable property names
+    breeds.forEach(function(breed) {
+      breedArray.push(breed)
+      //variable declared in global scope so have access in filter function
+      //resetting value didn't work, but pushing did
+    });
+    });
+
+}
+
+function displayBreeds() {
+  //extracted this from getBreeds() so can access breeds in other functions w/o reloading html
+  let listContainer = document.getElementById("dog-breeds");
+  //debugger;
+  console.log(breedArray)
+  // breedArray.forEach(function(breed){
+  //   let newLi = document.createElement("LI");
+  //   let text = document.createTextNode(breed); //w/o this get: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+  //   newLi.setAttribute("id", breed)
+  //   newLi.appendChild(text);
+  //   listContainer.appendChild(newLi)
+  // })
+}
+
+function onBreedClick() {
+    let listContainer = document.getElementById("dog-breeds");
+    listContainer.addEventListener('click', changeColor);
+  }
+
+  function changeColor(){
+    event.target.style.color = "pink";
+
+  }
+
+  function onFilterClick() {
+    let selectBreed = document.getElementById("breed-dropdown");
+    selectBreed.addEventListener('change', filterBreeds); //for dropdowns use change over click
+
+  }
+
+  function filterBreeds() {
+    let listContainer = document.getElementById("dog-breeds");
+    listContainer.innerHTML = ""; //need to reset to empty to load from scratch
+  }
